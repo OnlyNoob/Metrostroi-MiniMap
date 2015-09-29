7b8af892b20a9322688e143276d8330232b50d16
OnlyNoob <Pinotml@gmail.com>
2015-09-29 20:08:36 +0300

    New Orange Update
    -Added new orange track model and configs (Добавлена модель нового
    Оранжа и конфиг)

---
+++ addons/Minimap/lua/minimap/configs/maps/gm_mus_orange_line_long_h.lua
@@ -0,0 +1,29 @@
+--Map config--
+--set nil to use default value--
+if SERVER then
+	--Server side config--
+	--MiniMap minitrains offset--
+	MiniMap.MiniTrainsOffset = 0
+	MiniMap.Model = nil
+	MiniMap.Updated = true -- New signals, updated trains?
+--[[MiniMap.StationNames = {
+	[110] = "Garry's Mod Workers",
+	[111] = "VHE",
+	[112] = "Wallace Breen",
+	[113] = "GCFScape",
+	[114] = "Workshop",
+	[115] = "Park",
+	[116] = "Lithium",
+	[117] = "Glorious Country",
+	[200] = "Lithium",
+	[201] = "SENT Factory Station",
+	[202] = "Airport"}
+	MiniMap.SemiAutoSignals = {"D3","D4","D5","D6","D2 1","D2 3","D2 5","D2 7","D2 2","D2 4","D2 6","D2 8"}--]]
+	MiniMap.BlockSwitches = true
+else
+	--Client side config--
+	--MiniMap custom render bounds--
+	MiniMap.RenderBoundsAdd = Vector( 150, 150, 100 )
+	--MiniMap Panel render position offset--
+	MiniMap.PanelRenderOffset = Vector( 150, 80, 100 )
+end
