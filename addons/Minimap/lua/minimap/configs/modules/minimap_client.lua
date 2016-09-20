23c0c0a2cc7e7c7eead0696f395b59147417c3d1
OnlyNoob <Pinotml@gmail.com>
2016-09-20 23:18:08 +0300

    HotFix v4

--- addons/Minimap/lua/minimap/configs/modules/minimap_client.lua
+++
@@ -1,8 +0,0 @@
---MiniMap Client config--
---set nil to use default value--
-MiniMap.Client.Enabled = false --Enabled or Disabled? Global module switch.
-MiniMap.Client.Port = 5890 --Server port (5890 - Default)
-MiniMap.Client.Mode = "none" --Default 'none'. Another mods soon...
-MiniMap.Client.SupportedMaps = {
-["gm_mus_orange_metro_h"] = true
-}
--- addons/Minimap/lua/minimap/configs/modules/minimap_orangedisp.lua
+++
@@ -1,3 +0,0 @@
---MiniMap Orange Disp config--
---set nil to use default value--
-MiniMap.OrangeDisp.Enabled = true --Enabled or Disabled? Global module switch.
